13a3a36ef629f59d45850f3afaa377c512e7711c
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-07-06 11:05:41 +0630

    added my final output of zawgyi to uni

--- zg2uni.py
+++ zg2uni.py
@@ -9,7 +9,7 @@ def convert(input):
     #replace
     output = output.replace(u'\u106A', u'\u1009') #nyalay
     output = re.sub(u'\u1025(?=[\u1039\u102C])', u'\u1009', output) #nyalay yaychar
-    output = output.replace('u\u106B', u'\u100A') #nya
+    output = output.replace(u'\u106B', u'\u100A') #nya
     output = output.replace(u'\u1090', u'\u101B') #yagaut
     output = output.replace(u'\u1033', u'\u102F') #tachaung
     output = output.replace(u'\u1030', u'\u1034') #hnachaung
@@ -17,11 +17,13 @@ def convert(input):
     output = output.replace(u'\u103C', u'\u103D') #wa
     output = output.replace(u'\u1086', u'\u103F') #thagyi
     output = re.sub(u'[\u103B\u107E\u107F\u1080\u1081\u1082\u1083\u1084]', u'\u103C', output)#yayit
-    output = output.replace('u\u103A', u'\u103B') #rapint
-    output = output.replace('u\u1039', u'\u103A') #athet
-    output = output.replace('u\u104E', u'\u104E\u1044\u103A\u1038') #lagaung
-    output = output.replace('u\u1037\u1094\u1095', u'\u1037') #autkamyint
-    output = output.replace('u\u108F', u'\u1014') #nange
+    output = output.replace(u'\u103A', u'\u103B') #rapint
+    output = re.sub(u'\u1039', u'\u103A', output) #athet
+    output = output.replace(u'\u104E', u'\u104E\u1044\u103A\u1038') #lagaung
+    output = output.replace(u'\u1037\u1094\u1095', u'\u1037') #autkamyint
+    output = output.replace(u'\u108F', u'\u1014') #nange
+    output = re.sub(u'\u1088', u'\u103E + \u102F', output) #hahto_tachaung
+    output = re.sub(u'\u1089', u'\u103E + \u1030', output) #hatho_hnachaung
 
     #ngasint
     output = re.sub(u'([u1000-u1021])\u1064', u'\u1064\\1', output)
@@ -51,12 +53,19 @@ def convert(input):
     output = re.sub(u'\u107A', u'\u1039\u1017', output)#bachintsint
     output = re.sub(u'\u107B', u'\u1039\u1018', output)#basint
     output = re.sub(u'\u107C', u'\u1039\u1019', output)#masint
-    
+    output = re.sub(u'\u1085', u'\u1039\u101C', output)#lasint
+    output = re.sub(u'\u106E', u'\u100D\u1039\u100D', output)#tata-2lonesint
+    output = re.sub(u'\u106F', u'\u100E\u1039\u100D', output)#dayin_tasint
+    output = re.sub(u'\u1091', u'\u100F\u1039\u100D', output)#nagyi_dayinkaut_sint
+    output = re.sub(u'\u1092', u'\u100B\u1039\u100C', output)
+    output = re.sub(u'\u1097', u'\u100B\u1039\u100B', output)
  
     # reorder yayit to CMV format
     output = re.sub(u'(\u103C)([\u1000-\u1020])', u'\\2\\1', output)
     # reorder thawayhtoo
     output = re.sub(u'\u1031([\u1000-\u1021])', u'\\1\u1031', output)
     output = re.sub(u'\u1031([\u103B-\u103E\+])', u'\\1\u1031', output)
+    output = re.sub(u'\u105A', u'\u102B\ + \u103A', output)#yaycharshathto
+    
 
     return output
