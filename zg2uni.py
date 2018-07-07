87d65f72c6f951349119ffb9db5aae2a02641b66
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-07-07 19:58:16 +0630

    added my final output of zawgyi to uni

--- zg2uni.py
+++ zg2uni.py
@@ -2,28 +2,35 @@
 import re
 
 
-def convert(input):
-
+def replace(input):
+    # replace each zawgyi code point with equivalent code point in unicode
     output = input
 
-    #replace
+    
     output = output.replace(u'\u106A', u'\u1009') #nyalay
     output = re.sub(u'\u1025(?=[\u1039\u102C])', u'\u1009', output) #nyalay yaychar
-    output = output.replace(u'\u106B', u'\u100A') #nya
-    output = output.replace(u'\u1090', u'\u101B') #yagaut
-    output = output.replace(u'\u1033', u'\u102F') #tachaung
-    output = output.replace(u'\u1030', u'\u1034') #hnachaung
+    output = re.sub(u'\u106B', u'\u100A', output) #nya
+    output = re.sub(u'\u1090', u'\u101B', output) #yagaut
+    output = re.sub(u'\u1033', u'\u102F', output) #tachaung
+    output = re.sub(u'\u1030', u'\u1034', output) #hnachaung
     output = re.sub(u'[\u103D\u1087]', u'\u103E', output) #ha
-    output = output.replace(u'\u103C', u'\u103D') #wa
-    output = output.replace(u'\u1086', u'\u103F') #thagyi
+    output = re.sub(u'\u103C', u'\u103D', output) #wa
+    output = re.sub(u'\u1086', u'\u103F', output) #thagyi
     output = re.sub(u'[\u103B\u107E\u107F\u1080\u1081\u1082\u1083\u1084]', u'\u103C', output)#yayit
-    output = output.replace(u'\u103A', u'\u103B') #rapint
+    output = re.sub(u'\u103A', u'\u103B', output) #rapint
     output = re.sub(u'\u1039', u'\u103A', output) #athet
-    output = output.replace(u'\u104E', u'\u104E\u1044\u103A\u1038') #lagaung
-    output = output.replace(u'\u1037\u1094\u1095', u'\u1037') #autkamyint
-    output = output.replace(u'\u108F', u'\u1014') #nange
+    output = re.sub(u'\u104E', u'\u104E\u1044\u103A\u1038', output) #lagaung
+    output = re.sub(u'\u1037\u1094\u1095', u'\u1037', output) #autkamyint
+    output = re.sub(u'\u108F', u'\u1014', output) #nange
     output = re.sub(u'\u1088', u'\u103E + \u102F', output) #hahto_tachaung
     output = re.sub(u'\u1089', u'\u103E + \u1030', output) #hatho_hnachaung
+
+    return output
+
+
+def decompose(input):
+    #decomposed combined characters to sequenced characters
+    output = input
 
     #ngasint
     output = re.sub(u'([u1000-u1021])\u1064', u'\u1064\\1', output)
@@ -31,7 +38,8 @@ def convert(input):
     output = re.sub(u'([u1000-u1021])\u108c', u'\u1064\\1\u102E', output)
     output = re.sub(u'([u1000-u1021])\u108D', u'\u1064\\1\u1036', output)
     output = re.sub(u'([u1000-u1020])\u108E', u'\u102D\\1\u1036', output)
-    
+
+    #patsint
     output = re.sub(u'\u1060', u'\u1039\u1000', output)#kasint
     output = re.sub(u'\u1061', u'\u1039\u1001', output)#khasint
     output = re.sub(u'\u1062', u'\u1039\u1002', output)#gasint
@@ -59,13 +67,32 @@ def convert(input):
     output = re.sub(u'\u1091', u'\u100F\u1039\u100D', output)#nagyi_dayinkaut_sint
     output = re.sub(u'\u1092', u'\u100B\u1039\u100C', output)
     output = re.sub(u'\u1097', u'\u100B\u1039\u100B', output)
- 
+
+    return output
+
+def visual2logical(input):
+
+    output = input
     # reorder yayit to CMV format
     output = re.sub(u'(\u103C)([\u1000-\u1020])', u'\\2\\1', output)
     # reorder thawayhtoo
     output = re.sub(u'\u1031([\u1000-\u1021])', u'\\1\u1031', output)
     output = re.sub(u'\u1031([\u103B-\u103E\+])', u'\\1\u1031', output)
     output = re.sub(u'\u105A', u'\u102B\ + \u103A', output)#yaycharshathto
-    
+    output = re.sub(u'(\u102F)(\u102D\u102E])', u'\\2\\1', output)
+    output = re.sub(u'(\u1030)(\u102D\u102E])', u'\\2\\1', output)
+
+    return output
+
+
+
+def convert(input):
+
+    output = input
+
+
+    output = replace(output)
+    output = decompose(output)
+    output = visual2logical(output)
 
     return output
