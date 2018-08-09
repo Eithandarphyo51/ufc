70d90a4f23f99eae8a89e9d50c96673f093d44f9
Eithandarphyo51 <eithandarprogram@gmail.com>
2018-08-09 21:03:13 +0630

    added myresult for myanmar language

--- zg2uni.py
+++ zg2uni.py
@@ -28,13 +28,12 @@ def decompose(input):
     # decomposed combined characters to sequenced characters
     output = input
 
-    output = re.sub(u'\u1025(?=[\u103a\u102c])', u'\u1009', output)  # nyalay_yaychar
     ## nga_sint
     output = re.sub(u'([\u1000-\u1021])\u1064', u'\u1064\\1', output)
-    output = re.sub(u'([\u1000-\u1021])\u108b', u'\u1064\\1\u102d', output)
-    output = re.sub(u'([\u1000-\u1021])\u108c', u'\u1064\\1\u102e', output)
-    output = re.sub(u'([\u1000-\u1021])\u108d', u'\u1064\\1\u1036', output)
-    output = re.sub(u'\u108e', u'\u102d\u1036', output)
+    output = re.sub(u'([\u1000-\u1021])\u108b', u'\u1064\\1\u102d', output) #ngasint_with_Lonegyitin
+    output = re.sub(u'([\u1000-\u1021])\u108c', u'\u1064\\1\u102e', output) #ngasint_with_Lonegyitinsankhat
+    output = re.sub(u'([\u1000-\u1021])\u108d', u'\u1064\\1\u1036', output) #ngasint_with_thaythaytin
+    output = re.sub(u'([\u1000-\u1021])\u108e', u'\u102d\\1\u1036', output) #lonegyitin_with_thaythaytin
 
     output = re.sub(u'\u1088', u'\u103e\u102f', output)  # ha_toe and ta_chuang_ngin
     output = re.sub(u'\u1089', u'\u103e\u1030', output)  # ha_toe and na_chuang_ngin
@@ -42,33 +41,33 @@ def decompose(input):
     output = re.sub(u'\u108a', u'\u103d\u103e', output)  # waswe_hatoe
 
     # pr_sint
-    output = re.sub(u'\u1060', u'\u1039\u1000', output)  # ka_gyi
-    output = re.sub(u'\u1061', u'\u1039\u1001', output)  # ka_kway
-    output = re.sub(u'\u1062', u'\u1039\u1002', output)  # ga_nge
-    output = re.sub(u'\u1063', u'\u1039\u1003', output)  # ga_gyi
-    output = re.sub(u'\u1065', u'\u1039\u1005', output)  # sa_lone
-    output = re.sub(u'[\u1066\u1067]', u'\u1039\u1006', output)  # sa_lane
-    output = re.sub(u'\u1068', u'\u1039\u1007', output)  # sa_gwe
-    output = re.sub(u'\u1069', u'\u1039\u1008', output)  # sa_myin_zwe
-    output = re.sub(u'\u106c', u'\u1039\u100b', output)  # da_da_lin_gyake
-    output = re.sub(u'\u106d', u'\u1039\u100c', output)  # ta_wen_bae
+    output = re.sub(u'\u1060', u'\u1039\u1000', output)  # kagyi
+    output = re.sub(u'\u1061', u'\u1039\u1001', output)  # kakway
+    output = re.sub(u'\u1062', u'\u1039\u1002', output)  # gange
+    output = re.sub(u'\u1063', u'\u1039\u1003', output)  # gagyi
+    output = re.sub(u'\u1065', u'\u1039\u1005', output)  # salone
+    output = re.sub(u'[\u1066\u1067]', u'\u1039\u1006', output)  # salane
+    output = re.sub(u'\u1068', u'\u1039\u1007', output)  # zagwe
+    output = re.sub(u'\u1069', u'\u1039\u1008', output)  # zamyinzwe
+    output = re.sub(u'\u106c', u'\u1039\u100b', output)  # tatalin_gyake
+    output = re.sub(u'\u106d', u'\u1039\u100c', output)  # thawen_bae
     output = re.sub(u'\u106e', u'\u100d\u1039\u100d', output)  # da_yin_guat
     output = re.sub(u'\u106f', u'\u100d\u1039\u100e', output)  # da_yin_mot
     output = re.sub(u'\u1070', u'\u1039\u100f', output)  # na_gyi
-    output = re.sub(u'[\u1071\u1072]', u'\u1039\u1010', output)  # da_wen_bu
-    output = re.sub(u'[\u1073\u1074]', u'\u1039\u1011', output)  # ta_sin_htoo
-    output = re.sub(u'\u1075', u'\u1039\u1012', output)  # da_dway
-    output = re.sub(u'\u1076', u'\u1039\u1013', output)  # da_auat_chai
-    output = re.sub(u'\u1077', u'\u1039\u1014', output)  # nga_nge
-    output = re.sub(u'\u1078', u'\u1039\u1015', output)  # pa_saut
-    output = re.sub(u'\u1079', u'\u1039\u1016', output)  # pa_ot_top
-    output = re.sub(u'\u107a', u'\u1039\u1017', output)  # ba_lat_chai
+    output = re.sub(u'[\u1071\u1072]', u'\u1039\u1010', output)  # tawenpu
+    output = re.sub(u'[\u1073\u1074]', u'\u1039\u1011', output)  # htasinhtoo
+    output = re.sub(u'\u1075', u'\u1039\u1012', output)  # dadway
+    output = re.sub(u'\u1076', u'\u1039\u1013', output)  # daauat_chint
+    output = re.sub(u'\u1077', u'\u1039\u1014', output)  # ngange
+    output = re.sub(u'\u1078', u'\u1039\u1015', output)  # pasaut
+    output = re.sub(u'\u1079', u'\u1039\u1016', output)  # phaoat_htop
+    output = re.sub(u'\u107a', u'\u1039\u1017', output)  # bahtat_chint
     output = re.sub(u'[\u107b\u1093]', u'\u1039\u1018', output)  # ba_gone
     output = re.sub(u'\u107c', u'\u1039\u1019', output)  # ma
     output = re.sub(u'\u1085', u'\u1039\u101c', output)  # la
-    output = re.sub(u'\u1091', u'\u100f\u1039\u100d', output)  # ng&dyg
-    output = re.sub(u'\u1092', u'\u100b\u1039\u100c', output)  # ddlg&twb
-    output = re.sub(u'\u1097', u'\u100b\u1039\u100b', output)  # twiceddlg
+    output = re.sub(u'\u1091', u'\u100f\u1039\u100d', output)  # ng_with_dyg
+    output = re.sub(u'\u1092', u'\u100b\u1039\u100c', output)  # ttlg_with_twb
+    output = re.sub(u'\u1097', u'\u100b\u1039\u100b', output)  # ttlg_with_ttlg
 
     return output
 
